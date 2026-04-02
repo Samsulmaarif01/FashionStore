2388a4f0360bd3e6d0051d4200c4140003e4578c
Samsul Maarif <samsulmaarif1076@gmail.com>
2026-04-02 14:44:43 +0700

    fix: bug at item cart icon

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -97,11 +97,11 @@
                 </a>
 
                 <!-- Cart Icon -->
-                <a href="{{ route('cart.index') }}" class="relative text-gray-900 hover:text-indigo-600 transition-colors group">
+                <a href="{{ route('cart.index') }}" class="relative text-gray-900 hover:text-indigo-600 transition-colors group flex items-center justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:animate-wiggle" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.8">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                     </svg>
-                    <span class="absolute -top-1.5 -right-1.5 bg-indigo-600 text-white text-[9px] font-black h-4 w-4 rounded-full flex items-center justify-center ring-2 ring-white">
+                    <span class="absolute -top-2 -right-2 bg-indigo-600 text-white text-[9px] font-black h-4 w-4 rounded-full flex items-center justify-center ring-2 ring-white">
                         {{ count(session('cart', [])) }}
                     </span>
                 </a>
